18ada588234bc1066ac45f92da8525ffed9c1330
boomgreen87 <boomgreen87@yahoo.com>
2019-03-18 16:00:57 -0400

    Added close button and made banners animate

--- js/GOT_master.js
+++ js/GOT_master.js
@@ -5,10 +5,22 @@
 	// Grab the shields at the bottom of the page
 	const 	shields		= document.querySelectorAll('.sigil-container'),
 			lightBox 	= document.querySelector('.lightbox'),
-			video		= document.querySelector('video');
+			video		= document.querySelector('video'),
+			closeLB		= document.querySelector('.lightbox-close'),
+			banners 	= document.querySelector('#houseImages');
 
 	function showLightbox() {
+		// Grab the right video source
+		// Get the lowercase house name from the class list
+		let targetHouse = this.className.split(" ")[1];
+
+		// Make sure the names match - needs to be uppercase
+		// stark become Stark => First make capital S then add ark (for any class name)
+		let targetSrc = targetHouse.charAt(0).toUpperCase() + targetHouse.slice(1);
+		video.src = `video/House-${targetSrc}.mp4`;
+
 		lightBox.classList.add("show-lightbox");
+		video.load();
 		video.play();
 	}
 
@@ -20,7 +32,19 @@
 		video.pause();
 	}
 
-	shields.forEach(shield => shield.addEventListener("click", showLightbox));
+	function animateBanner() {
+		const offSet = 600; // This is the offset / width of one banner
+
+		// This is the total distance the images need to move as a pixel value
+		// data.offset is coming from each shield we click on.
+		totalOffset = this.dataset.offset * offSet + "px";
+		banners.style.right = totalOffset;
+	}
+
+	// shields.forEach(shield => shield.addEventListener("click", showLightbox));
+	shields.forEach(shield => shield.addEventListener("click", animateBanner));
 
 	video.addEventListener("ended", hideLightbox);
+
+	closeLB.addEventListener("click", hideLightbox);
 })();
